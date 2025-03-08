508627203cf97bb699d56881fd76bb0cce9d5ba2
quanquan <303730352@qq.com>
2025-03-08 13:05:58 +0800

    更新

--- Basic.HrService/Basic.HrGatewaryModular/Api/CompanyApi.cs
+++ Basic.HrService/Basic.HrGatewaryModular/Api/CompanyApi.cs
@@ -1,6 +1,7 @@
 ﻿using Basic.HrGatewaryModular.Interface;
 using Basic.HrGatewaryModular.Model.Company;
 using Basic.HrRemoteModel.Company.Model;
+using WeDonekRpc.ApiGateway.Attr;
 using WeDonekRpc.Helper.Validate;
 using WeDonekRpc.HttpApiGateway;
 
@@ -9,7 +10,7 @@ namespace Basic.HrGatewaryModular.Api
     internal class CompanyApi : ApiController
     {
         private readonly ICompanyService _Service;
-        public CompanyApi (ICompanyService service)
+        public CompanyApi ( ICompanyService service )
         {
             this._Service = service;
         }
@@ -18,7 +19,8 @@ namespace Basic.HrGatewaryModular.Api
         /// </summary>
         /// <param name="datum">公司资料</param>
         /// <returns></returns>
-        public long Add ([NullValidate("hr.company.datum.null")] CompanyAdd datum)
+        [ApiPrower("all", "hr.company.add")]
+        public long Add ( [NullValidate("hr.company.datum.null")] CompanyAdd datum )
         {
             return this._Service.AddCompany(datum);
         }
@@ -27,7 +29,7 @@ namespace Basic.HrGatewaryModular.Api
         /// 删除公司
         /// </summary>
         /// <param name="id">公司ID</param>
-        public void Delete ([NumValidate("hr.company.id.error", 1)] long id)
+        public void Delete ( [NumValidate("hr.company.id.error", 1)] long id )
         {
             this._Service.DeleteCompany(id);
         }
@@ -37,7 +39,7 @@ namespace Basic.HrGatewaryModular.Api
         /// </summary>
         /// <param name="id">公司ID</param>
         /// <returns></returns>
-        public CompanyDto Get ([NumValidate("hr.company.id.error", 1)] long id)
+        public CompanyDto Get ( [NumValidate("hr.company.id.error", 1)] long id )
         {
             return this._Service.GetCompany(id);
         }
@@ -47,7 +49,7 @@ namespace Basic.HrGatewaryModular.Api
         /// </summary>
         /// <param name="param">参数</param>
         /// <returns></returns>
-        public CompanyDto[] GetList ([NullValidate("hr.company.param.null")] UI_GetCompanyList param)
+        public CompanyDto[] GetList ( [NullValidate("hr.company.param.null")] UI_GetCompanyList param )
         {
             return this._Service.GetCompanyList(param.ParentId, param.IsAllChildren);
         }
@@ -57,7 +59,7 @@ namespace Basic.HrGatewaryModular.Api
         /// </summary>
         /// <param name="param">参数</param>
         /// <returns></returns>
-        public CompanyTree[] GetTree ([NullValidate("hr.company.param.null")] UI_GetCompanyTree param)
+        public CompanyTree[] GetTree ( [NullValidate("hr.company.param.null")] UI_GetCompanyTree param )
         {
             return this._Service.GetCompanyTree(param.ParentId, param.Status);
         }
@@ -67,7 +69,7 @@ namespace Basic.HrGatewaryModular.Api
         /// </summary>
         /// <param name="param">参数</param>
         /// <returns></returns>
-        public bool Set ([NullValidate("hr.company.param.null")] UI_SetCompany param)
+        public bool Set ( [NullValidate("hr.company.param.null")] UI_SetCompany param )
         {
             return this._Service.SetCompany(param.Id, param.Datum);
         }
